3beed0b0a4568dc5744f2b5da5161d804f72c5fd
matin <150115043+iiMattheww@users.noreply.github.com>
2025-09-28 23:56:11 +0330

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const descreption = reactDescriptions[genRandomInt(2)];
+    return (
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {descreption} React concepts you will need for almost any app you
+                are going to build!
+            </p>
+        </header>
+    );
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
