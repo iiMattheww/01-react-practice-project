760a1d4263c6b672be198d94c09db63a2d6be25d
matin <150115043+iiMattheww@users.noreply.github.com>
2025-10-08 15:52:31 +0330

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import Examples from "./components/Examples.jsx";
-/
+
 function App() {
     return (
         <>
